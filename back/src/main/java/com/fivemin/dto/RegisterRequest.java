91fbe8fea7415763fab83e0ccbc40045319caaab
jaehwan0706 <jaehwan0706@gmail.com>
2026-05-28 10:45:05 +0900

    백엔드 세팅

---
+++ back/src/main/java/com/fivemin/dto/RegisterRequest.java
@@ -0,0 +1,9 @@
+package com.fivemin.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RegisterRequest {
+    private String email;
+    private String password;
+}
---
+++ back/src/main/java/com/fivemin/entity/Role.java
@@ -0,0 +1,5 @@
+package com.fivemin.entity;
+
+public enum Role {
+    USER, ADMIN
+}
